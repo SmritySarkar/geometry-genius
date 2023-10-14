3a4e345c3eb9bdc4b76e6a2df15e43ee28580111
Smrity Sarkar <smritysarkarsmrity@gmail.com>
2023-10-15 01:25:09 +0600

    Applied input validation for numbers

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -2,11 +2,17 @@ function calculateTriangleArea(){
     // Get Triangle Base Value
     const base = getInputValue('triangle-base');
     const height = getInputValue('triangle-height');
+    if(isNaN(base) || isNaN(height)){
+        alert('Please insert a number');
+        return;
+    }
     // Calculate Area 
     const area = 0.5 * base * height;
     // Show Calculated Value
     setElementInnerText('triangle-area', area);
+    addToCalculationEntry('Triangle', area);
 }
+
 
 function calculateRectangleArea(){
     // Get Rectangle Width Value
@@ -16,7 +22,7 @@ function calculateRectangleArea(){
     const area = width * length;
     // Show the calculated Rectangle Area
     setElementInnerText('rectangle-area', area);
-
+    addToCalculationEntry('Rectangle', area);
 }
 
 function calculateParallelogramArea(){
@@ -24,6 +30,7 @@ function calculateParallelogramArea(){
     const height = getInputValue('parallelogram-height');
     const area = base * height;
     setElementInnerText('parallelogram-area', area);
+    addToCalculationEntry('Parallelogram', area);
 }
 
 function calculateRhombusArea(){
@@ -31,7 +38,7 @@ function calculateRhombusArea(){
     const d2 = getInputValue('diagonal-2');
     const area = 0.5 * d1 * d2;
     setElementInnerText('rhombus-area', area);
-
+    addToCalculationEntry('Rhombus', area);
 }
 
 function calculatePentagonArea(){
@@ -39,13 +46,16 @@ function calculatePentagonArea(){
     const apothem = getInputValue('apothem');
     const area = 0.5 * perimeter * apothem;
     setElementInnerText('pentagon-area', area);
+    addToCalculationEntry('Pentagon', area);
 }
 
 function calculateEllipseArea(){
     const majorRadius = getInputValue('first-radius');
     const minorRadius = getInputValue('second-radius');
     const area = 3.14 * majorRadius * minorRadius;
-    setElementInnerText('ellipse-area', area);
+    const areaTwoDecimal = area.toFixed(2);
+    setElementInnerText('ellipse-area', areaTwoDecimal);
+    addToCalculationEntry('Ellipse', area);
 }
 
 // Reusable function --> reduce duplicate code
@@ -64,3 +74,13 @@ function setElementInnerText(elementId, area){
     element.innerText = area;
 }
 
+// Add to calculation entry
+function addToCalculationEntry(areaType, area){
+    const calculationEntry = document.getElementById('calculation-entry');
+    const count = calculationEntry.childElementCount;
+    const p = document.createElement('p');
+    p.classList.add('my-4');
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-success btn-sm" >Convert</button>`;
+    calculationEntry.appendChild(p);
+
+}
